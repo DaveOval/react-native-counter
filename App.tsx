64adeaa2134d86fb26d53246fb000e5084906ac1
David Vazquez <dave_u@outlook.com>
2023-09-26 23:07:26 -0600

    daw!

--- App.tsx
+++ App.tsx
@@ -1,15 +1,17 @@
 import React from 'react'
 import { BoxObjectModelScreen } from './src/screens/BoxObjectModelScreen';
 import { SafeAreaView } from 'react-native';
+import { DimensionesScreen } from './src/screens/DimensionesScreen';
 // import { HolaMundoScreen } from './src/screens/HolaMundoScreen';
 //import { ContadorScreen } from './src/screens/ContadorScreen';
 
 const App = () => {
   return (
-    <SafeAreaView>
+    <SafeAreaView style={{flex:1}}>
       {/* <HolaMundoScreen/>
       <ContadorScreen /> */}
-      <BoxObjectModelScreen />
+      {/* <BoxObjectModelScreen /> */}
+      <DimensionesScreen />
     </SafeAreaView>
   )
 }
